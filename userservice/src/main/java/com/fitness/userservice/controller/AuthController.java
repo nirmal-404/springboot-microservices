cc0b0741c1177931955198bfc6973f59ee9941fd
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-05-24 05:50:11 +0530

    userservice microservice basic implementations

---
+++ userservice/src/main/java/com/fitness/userservice/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.fitness.userservice.controller;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId){
+        return ResponseEntity.ok(userService.getUserProfile(userId));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request){
+        return ResponseEntity.ok(userService.register(request));
+    }
+
+//    @PostMapping("/login")
+//    public ResponseEntity<> login(){
+//        return ""
+//    }
+
+}
